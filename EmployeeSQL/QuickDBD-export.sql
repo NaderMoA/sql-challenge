b011123ccde76784b5fb7bd5525e0ac8b84945ba
NaderMoa <nadermoazezi@gmail.com>
2024-01-10 01:23:20 -0500

    Update QuickDBD-export.sql

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -1,7 +1,7 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-alter database "Module 9 Assignment" set datestyle to "ISO,MDY"
+
 CREATE TABLE "Departments" (
     "dept_no" varchar   NOT NULL,
     "dept_name" varchar   NOT NULL,
