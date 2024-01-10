3cf27cd229f9db9835f378ac916e7fbf33780956
NaderMoa <nadermoazezi@gmail.com>
2024-01-10 18:30:30 -0500

    Update Queries_codes.sql

--- EmployeeSQL/Queries_codes.sql
+++ EmployeeSQL/Queries_codes.sql
@@ -1,83 +1,83 @@
 --Q1 
-Select "Employees".emp_no,
-"Employees".first_name,
-"Employees".last_name,
-"Employees".sex,
+Select employees.emp_no,
+employees.first_name,
+employees.last_name,
+employees.sex,
 "Salaries".salary
-From "Employees"
+From employees
 Inner Join "Salaries"
-on "Salaries".emp_no = "Employees".emp_no
+on "Salaries".emp_no = employees.emp_no
 
 --Q2
-select "Employees".first_name,
-"Employees".last_name,
-"Employees".hire_date
-from "Employees"
+select employees.first_name,
+employees.last_name,
+employees.hire_date
+from employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1986
 
 --Q3
-select "Dept_manager".emp_no,
-"Dept_manager".dept_no,
-"Departments".dept_name,
-"Employees".first_name,
-"Employees".last_name,
-"Titles".title
-from "Dept_manager"
-inner join "Departments"
-on "Dept_manager".dept_no = "Departments".dept_no
-inner join "Employees"
-on "Employees".emp_no = "Dept_manager".emp_no
-inner join "Titles"
-on "Titles".title_id = "Employees".emp_title_id
+select dept_manager.emp_no,
+dept_manager.dept_no,
+departments.dept_name,
+employees.first_name,
+employees.last_name,
+titles.title
+from dept_manager
+inner join departments
+on dept_manager.dept_no = departments.dept_no
+inner join employees
+on employees.emp_no = dept_manager.emp_no
+inner join titles
+on titles.title_id = employees.emp_title_id
 
 --Q4
-select "Employees".emp_no,
-"Employees".first_name,
-"Employees".last_name,
-"Dept_emp".dept_no,
-"Departments".dept_name
-FROM "Employees"
-INNER JOIN "Dept_emp"
-on "Dept_emp".emp_no = "Employees".emp_no
-INNER Join "Departments"
-on "Departments".dept_no = "Dept_emp".dept_no
+select employees.emp_no,
+employees.first_name,
+employees.last_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees
+INNER JOIN dept_emp
+on dept_emp.emp_no = employees.emp_no
+INNER Join departments
+on departments.dept_no = dept_emp.dept_no
 
 --Q5
-SELECT "Employees".first_name,
-"Employees".last_name,
-"Employees".sex
-FROM "Employees"
-WHERE "Employees".first_name = 'Hercules' and 
-"Employees".last_name LIKE 'B%'
+SELECT employees.first_name,
+employees.last_name,
+employees.sex
+FROM employees
+WHERE employees.first_name = 'Hercules' and 
+employees.last_name LIKE 'B%'
 
 --Q6
-SELECT "Dept_emp".emp_no,
-"Departments".dept_name,
-"Employees".last_name,
-"Employees".first_name
-FROM "Dept_emp"
-INNER JOIN "Departments"
-on "Departments".dept_no = "Dept_emp".dept_no
-INNER JOIN "Employees"
-on "Dept_emp".emp_no = "Employees".emp_no
-WHERE "Dept_emp".dept_no IN (SELECT "Departments".dept_no from "Departments"
-WHERE "Departments".dept_name = 'Sales');
+SELECT dept_emp.emp_no,
+departments.dept_name,
+employees.last_name,
+employees.first_name
+FROM dept_emp
+INNER JOIN departments
+on departments.dept_no = dept_emp.dept_no
+INNER JOIN employees
+on dept_emp.emp_no = employees.emp_no
+WHERE dept_emp.dept_no IN (SELECT departments.dept_no from departments
+WHERE departments.dept_name = 'Sales');
 
 --Q7
-SELECT "Employees".emp_no,
-"Employees".last_name,
-"Employees".first_name,
-"Departments".dept_name
-FROM "Employees"
-INNER JOIN "Dept_emp"
-on "Employees".emp_no = "Dept_emp".emp_no
-INNER JOIN "Departments"
-on "Departments".dept_no = "Dept_emp".dept_no
-WHERE "Departments".dept_name = 'Sales' OR  "Departments".dept_name = 'Development'
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM employees
+INNER JOIN dept_emp
+on employees.emp_no = dept_emp.emp_no
+INNER JOIN departments
+on departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR  departments.dept_name = 'Development'
 
 --Q8
-SELECT "Employees".last_name,
-COUNT("Employees".last_name)as frequency
-FROM "Employees"
-GROUP BY "Employees".last_name
+SELECT employees.last_name,
+COUNT(employees.last_name)as frequency
+FROM employees
+GROUP BY employees.last_name
 ORDER BY frequency Desc
