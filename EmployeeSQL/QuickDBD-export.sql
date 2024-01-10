5ae77b9004ec61291e26d89c38b5471172be92a8
NaderMoa <nadermoazezi@gmail.com>
2024-01-10 18:26:53 -0500

    Update QuickDBD-export.sql

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -2,17 +2,18 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
+
 CREATE TABLE "Departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
+    "dept_no" varchar(5)   NOT NULL,
+    "dept_name" varchar(20)   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "Titles" (
-    "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
+    "title_id" varchar(20)   NOT NULL,
+    "title" varchar(20)   NOT NULL,
     CONSTRAINT "pk_Titles" PRIMARY KEY (
         "title_id"
      )
@@ -20,11 +21,11 @@ CREATE TABLE "Titles" (
 
 CREATE TABLE "Employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" varchar   NOT NULL,
+    "emp_title_id" varchar(20)   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "sex" varchar   NOT NULL,
+    "first_name" varchar(15)   NOT NULL,
+    "last_name" varchar(25)   NOT NULL,
+    "sex" varchar(2)   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
@@ -33,14 +34,14 @@ CREATE TABLE "Employees" (
 
 CREATE TABLE "Dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
+    "dept_no" varchar(5)   NOT NULL,
     CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
 CREATE TABLE "Dept_manager" (
-    "dept_no" varchar   NOT NULL,
+    "dept_no" varchar(5)   NOT NULL,
     "emp_no" int   NOT NULL,
     CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
@@ -72,4 +73,9 @@ REFERENCES "Employees" ("emp_no");
 
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
-
+ALTER TABLE "Departments" RENAME TO Departments;
+ALTER TABLE "Titles" RENAME TO Titles;
+ALTER TABLE "Employees" RENAME TO Employees;
+ALTER TABLE "Dept_emp" RENAME TO Dept_emp;
+ALTER TABLE "Dept_manager" RENAME TO Dept_manager;
+ALTER TABLE "Salaries" RENAME TO Salaries;
